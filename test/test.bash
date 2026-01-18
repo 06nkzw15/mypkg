bddff0ea2b6b4f05ccc68f6d09051f581838932b
Taiki Nakazawa <nakazawa15623@gmail.com>
2026-01-18 15:35:20 +0900

    change test.bash

--- test/test.bash
+++ test/test.bash
@@ -10,11 +10,9 @@ source /opt/ros/humble/setup.bash
 source install/setup.bash
 
 colcon build
-colcon test
+
 timeout 10 ros2 launch mypkg stdin_publisher.launch.py > /tmp/mypkg.log 2>&1 || true
 
 cat /tmp/mypkg.log
 
 grep -q "Type something and press Enter" /tmp/mypkg.log
-
-exit 0
